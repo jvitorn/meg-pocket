924c707ac650bebb5ad527bc67d84309ce1b447a
João Vitor Novais <41808876+jvitorn@users.noreply.github.com>
2025-10-30 15:49:27 +0000

    resolvendo problema do next.config

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.SUPABASE_PROJECT_ID + '.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
-      },
-      // Ou usando a URL completa
-      {
-        protocol: 'https',
-        hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL || '').hostname,
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_URL ? 
+          new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname : 
+          'krxuafiolrihvoajvmnc.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
